5efcd5fd621f04440a853f9bca9e394c8644ce2c
Luke Webb <lukewebb2000@gmail.com>
2025-01-30 20:41:38 +1300

    Create User and Post Models and controllers.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_30_051832) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_30_074038) do
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
